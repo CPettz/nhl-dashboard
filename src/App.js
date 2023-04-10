7878e754765d7448348c015eee1d0e45c0c6ca6d
Christian Pettigrew <christian.pettigrew123@gmail.com>
2023-04-10 13:44:03 -0400

    Extracted team data from the teams endpoint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect, useCallback } from "react";
 import Card from "./components/UI/Card";
 
+const NHL_TEAMS_URL = "https://statsapi.web.nhl.com/api/v1/teams";
+
 function App() {
+
+  const fetchTeamsHandler = useCallback(async () => {
+
+    try {
+      const response = await fetch(NHL_TEAMS_URL);
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!")
+      }
+
+      const teamsData = await response.json();
+
+      const nhl_teams = [];
+      
+      for (const key in teamsData.teams) {
+        nhl_teams.push({
+          id: teamsData.teams[key].id,
+          teamName: teamsData.teams[key].name
+        })
+      }
+
+      console.log(nhl_teams)
+
+    } catch (error) {
+      console.log(error)
+    }
+
+  }, []);
+
+  useEffect(() => {
+    fetchTeamsHandler();
+  }, [fetchTeamsHandler]);
+
   return (
     <Card>
       <h1 className="text-3xl">NHL Dashboard</h1>
